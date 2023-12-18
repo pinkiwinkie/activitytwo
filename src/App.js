8301122df52fa713c924f3813a7def5f52db7e57
pinkiwinkie <elrumor99@gmail.com>
2023-12-18 18:35:10 +0100

    ejer finish

--- src/App.js
+++ src/App.js
@@ -13,6 +13,22 @@ function App() {
         setNumClicks(numClicks+1);
     };
 
+    const menos1 =()=>{
+        //console.log('click');
+        setNumClicks(numClicks-1);
+    };
+
+    const mas10 =()=>{
+        //console.log('click');
+        setNumClicks(numClicks+10);
+    };
+
+
+    const menos10 =()=>{
+        //console.log('click');
+        setNumClicks(numClicks-10);
+    };
+
     const resetCounter = ()=>{
         //console.log('reset');
         setNumClicks(0);
@@ -25,10 +41,24 @@ function App() {
         </div> 
         <div className='container-main'>
             <Counter numClicks={numClicks}/>
+            <div className='buttons-container'>
             <Button
-                text='click'
+                text='+1'
                 isButtonClick={true}
                 gestionClick={gestionClick}/>
+            <Button
+                text='-1'
+                isButtonClick={true}
+                gestionClick={menos1}/>
+            <Button
+                text='+10'
+                isButtonClick={true}
+                gestionClick={mas10}/>
+            <Button
+                text='-10'
+                isButtonClick={true}
+                gestionClick={menos10}/>
+            </div>
             <Button
                 text='Reset'
                 isButtonClick={false}
